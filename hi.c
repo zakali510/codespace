c118ed54835bd943d527cf2487597ab5966692f6
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-02-26 06:47:51 +0000

    Sun, Feb 26, 2023, 6:47 AM +00:00

--- hi.c
+++ hi.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
     string name = get_string("What is your name? ");
-
-    int n = 0;
-    while (name[n] != '\0')
-    {
-	n++;
-    }
+    int n = strlen(name);
     printf("%i\n", n);
 }
