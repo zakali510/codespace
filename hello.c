892b1c266d8d5620961c47a68d4173e8450bfac5
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-02-26 06:09:19 +0000

    Sun, Feb 26, 2023, 6:09 AM +00:00

--- hello.c
+++ hello.c
@@ -1,2 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
+
+const int N = 3;
+
+float average(int array[]);
+
+int main(void)
+{
+    int scores(N);
+    for (int i = 0; i < N; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
+}
+
+float average(int array[])
+int sum = 0;
