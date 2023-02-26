d2562ccd4dc760b19920c9fe8d3649c41d10379e
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-02-26 06:16:42 +0000

    Sun, Feb 26, 2023, 6:16 AM +00:00

--- hello.c
+++ hello.c
@@ -6,7 +6,7 @@ const int N = 3;
 float average(int array[]);
 
 int main(void)
-{
+{ // this prints out Score: three times
     int scores[N];
     for (int i = 0; i < N; i++)
     {
@@ -15,7 +15,8 @@ int main(void)
     printf("Average Score: %f\n", average(scores));
 }
 
-float average(int array[])
+float average(int array[]) // this gets the average of the score,
+                           // and divides it by three
 {
     int sum = 0;
     for (int i = 0; i < N; i++)
