7bfb53d7cd2a352f6eaf1788a88a526f4fbbcc3e
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-02-26 06:28:47 +0000

    Sun, Feb 26, 2023, 6:28 AM +00:00

--- hi.c
+++ hi.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
+    string s = "HI!";
 
-    printf("%i %i %i \n", c1, c2, c3);
+    printf("%c\n", s[0]);
 }
