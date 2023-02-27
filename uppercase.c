b297a5bb25b2a7e6fb8ed534e33c4403c16c0c10
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-02-27 22:02:07 +0000

    Mon, Feb 27, 2023, 10:02 PM +00:00

--- uppercase.c
+++ uppercase.c
@@ -8,16 +8,8 @@ int main(void)
     string s = get_string("Before: ");
     printf("After: ");
     for (int i = 0; i < strlen(s); i++) // a loop that repeats for every character inputted by user
-    {
-        if (islower(s[i]) ) // checks if character is lowercase
         {
             printf("%c", toupper(s[i])); // converts all characters from lowercase to uppercase
         }
-        else
-        {
-            printf("%c", isupper(s[i])); // if the characters aren't lowercased it'll convert to uppercase
-        }
-
-    }
     printf("\n");
 }
