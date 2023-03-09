4e234ef4d4535bee7a1658e1db858e7caa80e2c4
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-03-09 17:27:12 +0000

    Thu, Mar 9, 2023, 5:27 PM +00:00

--- uppercase.c
+++ uppercase.c
@@ -6,9 +6,10 @@
 int main(void)
 {
     string s = get_string("Before ");
+    printf("After: ");
     for (int i = 0; i < strlen(s); i++)
     if (s[i] >= 'a' &&  s[i] <= 'z')
     {
-        printf("c", s[i] - 32);
+        printf("%c", s[i] - 32);
     }
 }
