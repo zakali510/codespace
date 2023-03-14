19c80dccb50a302cea1571c3be45fb8ede083e70
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-03-14 04:26:58 +0000

    Tue, Mar 14, 2023, 4:26 AM +00:00

--- greet.c
+++ greet.c
@@ -11,6 +11,6 @@ int main(int argc, string argv[])
     {
         printf("hello, world\n");
     }
-    
+
 
 }
---
+++ status.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Missing command-line argument\n");
+
+    }
+    else
+    {
+        printf("hello, %s\n", argv[1]);
+    }
+}
