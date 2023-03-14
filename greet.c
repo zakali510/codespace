ece737a20de385af64abfae86892b9daca2884c8
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-03-14 04:15:06 +0000

    Tue, Mar 14, 2023, 4:15 AM +00:00

--- greet.c
+++ greet.c
@@ -3,5 +3,14 @@
 
 int main(int argc, string argv[])
 {
-    printf("hello, %s\n", argv[1]);
+    if (argc == 2)
+    {
+        printf("hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("hello, world\n");
+    }
+    
+
 }
