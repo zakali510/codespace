3cf0c1d1117f3d263fb4bcbd4058d0d0fcb8d2e1
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-03-14 04:29:49 +0000

    Tue, Mar 14, 2023, 4:29 AM +00:00

--- status.c
+++ status.c
@@ -6,10 +6,11 @@ int main(int argc, string argv[])
     if (argc != 2)
     {
         printf("Missing command-line argument\n");
-
+        return 1;
     }
     else
     {
         printf("hello, %s\n", argv[1]);
+        return 0;
     }
 }
