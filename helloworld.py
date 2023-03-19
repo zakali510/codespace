1c12dfb3c07cc61eca76cf3878c81e45e263f460
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-03-19 21:11:36 +0000

    Sun, Mar 19, 2023, 9:11 PM +00:00

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
+
 name = input("What is your name? ")
 
-name = name.strip() # this is a method using name of string, and name of functiion after .
-name = name.title()
+name = name.strip().title()
 
 print(f"Hello {name}")
