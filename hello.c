ab137fc050380c6b5a506ddfe5325475d296ccf6
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-04-03 16:39:40 +0000

    Mon, Apr 3, 2023, 4:39 PM +00:00

--- hello.c
+++ hello.c
@@ -4,32 +4,31 @@
 
 const int N = 3;
 
-float average(int array[]); // creates a function named average that takes an array of integers
+float average(int array[]);
 
 int main(void)
-{ // this prints out an array[] of Score: three times
-    int scores[N]; // const N = 3, repeats 3 times ((scores is name of array)) LOCAL DECLARATION
-    for (int i = 0; i < N; i++)
-    {
-        scores[N] = get_int("Score: "); // i repeats 3 times because const int N = 3
-    }
-    if (scores[N] != isdigit())
-    {
-        do int scores[N];
-    }
-    printf("Average Score: %f\n", average(scores)); // average (scores) scores is array
-
-}
-
-
-
-float average(int array[]) // this gets the average of the score,
-                           // and divides it by three
 {
-    int sum = 0; // sets sum at 0 and retrieves the value of all scores and divide by 3
+    int scores[N];
     for (int i = 0; i < N; i++)
     {
-        sum+=array[i];
+        scores[i] = get_int("Score: ");
+        while (!isdigit(getchar()))
+        {
+            printf("Invalid input. Please enter a digit.\n");
+            return 1;
+        }
     }
-    return sum / (float) 3;
+    printf("Average Score: %f\n", average(scores));
+    return 0;
 }
+
+float average(int array[])
+{
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += array[i];
+    }
+    return sum / (float) N;
+}
+
