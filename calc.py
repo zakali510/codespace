142de0e384e75ad63d68413a225d4534382d409d
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-04-06 23:06:16 +0000

    Thu, Apr 6, 2023, 11:06 PM +00:00

--- calc.py
+++ calc.py
@@ -1,4 +1,14 @@
-x = input("First number ")
-y = input("Second number ")
+def main():
+  x = int(input("What's x: "))
+  if is_even(x):
+    print("Even")
+  else:
+    print("Odd")
 
-print (int(x) + int(y))
+
+def is_even(n):
+  if n % 2 == 0:
+    return True
+  else:
+    return False
+main()
