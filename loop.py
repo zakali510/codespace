f32a4a4859efafb7e115a384e313d13d38bdf005
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-04-20 21:10:22 +0000

    Thu, Apr 20, 2023, 9:10 PM +00:00

--- loop.py
+++ loop.py
@@ -1,4 +1,8 @@
-students = {"Name": "Zak", "Grade": "11th Grade", "Age": "16"}
+students = [
+{"Name": "Zak", "Grade": "11th Grade", "Age": "16"},
+{"Name": "Bob", "Grade": "10th Grade", "Age": "15"},
+{"Name": "George", "Grade": "12th Grade", "Age": "17"}
+]
 
 for student in students:
-    print(student["name"])
+    print(students["1"], students["Grade"], sep = " is in ")
