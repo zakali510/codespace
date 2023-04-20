2364af661460ef9c86ca8482c6430525e3ec1d02
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-04-20 21:04:54 +0000

    Thu, Apr 20, 2023, 9:04 PM +00:00

--- loop.py
+++ loop.py
@@ -1,4 +1,4 @@
-students = ["Name": "Zak" "Grade": "11th Grade" "Age": "16"]
+students = {"Name": "Zak", "Grade": "11th Grade", "Age": "16"}
 
 for student in students:
-    print(student"[name]")
+    print(student["name"])
