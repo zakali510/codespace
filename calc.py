90cf6e49090dffbbd99a7d2ab82859ede6f1c0a3
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-04-26 16:19:35 +0000

    Wed, Apr 26, 2023, 4:19 PM +00:00

--- calc.py
+++ calc.py
@@ -16,7 +16,4 @@ def is_even(n):
     return False
 
 main()
-#Using a try-except block to catch the ValueError raised when int() is called on a non-integer input.
-#Printing an error message and calling main() recursively if an invalid input is provided.
-#Removing the if x != int() line, which is unnecessary and causes a NameError because x is not defined outside the main() function.
 
