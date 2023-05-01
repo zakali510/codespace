2dcd3c1d46b713de380f68d02f906fed8892d850
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-05-01 15:45:28 +0000

    Mon, May 1, 2023, 3:45 PM +00:00

--- hi.py
+++ hi.py
@@ -1,8 +1,14 @@
-while True:
-    try:
-        x = int(input("What is x? "))
-    except ValueError:
-        print("Please input an integer")
-    else:
-        print(f"x is {x}")
-        break
+def main():
+    x = get_int()
+
+
+def get_int():
+    while True:
+        try:
+            x = int(input("What is x? "))
+        except ValueError:
+            print("Please input an integer")
+        else:
+            print(f"x is {x}")
+            break
+    return x
