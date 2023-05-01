6b009066e57d32f780a07eb2f0d486ed32de9669
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-05-01 20:58:26 +0000

    Mon, May 1, 2023, 8:58 PM +00:00

--- recursive.py
+++ recursive.py
@@ -1,10 +1,12 @@
-
+def recursive3():
     def mult3(n):
         if n == 0:
-            return 2
+            return 0
         else:
             return mult3(n-1) + 3
 
     for i in range(0,10):
         print(mult3(i))
+
+        
 
