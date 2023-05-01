c17bb0e5954ac305a1f27853c6eebcb6204c004a
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-05-01 20:54:38 +0000

    Mon, May 1, 2023, 8:54 PM +00:00

--- recursive.py
+++ recursive.py
@@ -3,7 +3,7 @@ def even_numbers():
         if n == 0:
             return 2
         else:
-            return mult3(n-1) + 2
+            return mult3(n-1) + 3
 
     for i in range(0,10):
         print(mult3(i))
