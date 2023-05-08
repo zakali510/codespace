b654a32323b5cca22d96a8e28646989ed939d7ba
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-05-08 16:18:25 +0000

    Mon, May 8, 2023, 4:18 PM +00:00

--- say.py
+++ say.py
@@ -1,3 +1,8 @@
-import sys
 import requests
+import sys
 
+if len(sys.argv) != 2:
+    sys.exit()
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+print(response.json())
