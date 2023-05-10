8d51a34bbdbc28fd8d677c1d4bbc45fb89e21e54
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-05-10 19:34:00 +0000

    Wed, May 10, 2023, 7:34 PM +00:00

--- testt.py
+++ testt.py
@@ -6,4 +6,8 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
-print(response.json())
+
+
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"])
