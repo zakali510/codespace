c1625b3d30044d20161d2a87f65c435cc976efd7
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-05-10 19:32:19 +0000

    Wed, May 10, 2023, 7:32 PM +00:00

--- testt.py
+++ testt.py
@@ -5,5 +5,5 @@ import sys
 if len(sys.argv) != 2:
     sys.exit()
 
-response = requests.get("https://www.youtube.com/@" + sys.argv[1])
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
 print(response.json())
