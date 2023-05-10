f99f858b7f8ab91fe82e44b066df516224de5ef3
zakali510 <124641956+zakali510@users.noreply.github.com>
2023-05-10 19:30:45 +0000

    Wed, May 10, 2023, 7:30 PM +00:00

--- testt.py
+++ testt.py
@@ -6,7 +6,4 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get("https://www.youtube.com/@" + sys.argv[1])
-
-o = response.json()
-for result in o["results"]:
-    print(result["trackName"])
+print(response.json())
